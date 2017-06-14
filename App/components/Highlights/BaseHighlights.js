2d6e71b7b44845e8c30055381cc9c2b52a708cb2
dillerm <dillerm@oregonstate.edu>
2017-06-14 15:31:08 -0700

    added jungle and roaming, and added icons to lanes story

--- App/components/Highlights/BaseHighlights.js
+++ App/components/Highlights/BaseHighlights.js
@@ -1,31 +1,52 @@
+import {
+	StyleSheet,
+	Image,
+	Text,
+	View,
+	ScrollView,
+	TouchableOpacity,
+	Button,
+	Linking,
+} from 'react-native';
+
 
 export const formatTemplate = (template, dict) => {
-  if (!template) {
-    return ["invalid template"];
-  }
-  const pattern = /(\{[^}]+\})/g;
-  let result = template.split(pattern);
-  for (let i = 0; i < result.length; i += 1) {
-    if (result[i].match(pattern) && result[i].slice(1, -1) in dict) {
-      result[i] = dict[result[i].slice(1, -1)];
-    }
-  }
-  result = result.filter(part => part !== '');
-  return result;
+	if (!template) {
+		return ["invalid template"];
+	}
+	const pattern = /(\{[^}]+\})/g;
+	let result = template.split(pattern);
+	for (let i = 0; i < result.length; i += 1) {
+		if (result[i].match(pattern) && result[i].slice(1, -1) in dict) {
+			result[i] = dict[result[i].slice(1, -1)];
+		}
+	}
+	result = result.filter(part => part !== '');
+	return result;
 };
 
 // Enumerates a list of items using the correct language syntax
 export const formatList = (items, noneValue = []) => {
-  switch (items.length) {
-    case 0:
-      return noneValue;
-    case 1:
-      return items;
-    case 2:
-      return formatTemplate(`${items[0]} and ${items[1]}`);
-    case 3:
-      return formatTemplate(`${items[0]}, ${items[1]}, and ${items[2]}`);
-    default:
-      return formatTemplate(`${items.shift()}, ${formatList(items)}`);
-  }
+	switch (items.length) {
+		case 0:
+			return noneValue;
+		case 1:
+			return items;
+		case 2:
+			return formatTemplate("{0} and {1}", { 0: items[0], 1: items[1] });
+		case 3:
+			return formatTemplate("{0}, {1}, and {2}", { 0: items[0], 1: items[1], 2: items[2] });
+		default:
+			return formatTemplate("{0}, {rest}", { 0: items.shift(), rest: formatList(items) });
+	}
 };
+
+export const styles = StyleSheet.create({
+	highlight: {
+		marginTop: 10,
+		backgroundColor: '#f5fcff',
+	},
+	header: {
+		fontSize: 20,
+	}
+})
--- App/components/Highlights/Lanes.js
+++ App/components/Highlights/Lanes.js
@@ -10,7 +10,8 @@ import {
 } from 'react-native';
 import {
 	formatTemplate,
-	formatList
+	formatList,
+	styles
 } from './BaseHighlights.js';
 import heroes from 'dotaconstants/build/heroes.json';
 
@@ -20,83 +21,121 @@ const ODOTA_API = "https://api.opendota.com";
 
 
 const PlayerSpan = player => {
-  return heroes[player.hero_id].localized_name;
+	return [<Image 
+				style={{width: 48, height: 48}}
+				source={{ uri: `${ODOTA_API}${heroes[player.hero_id].icon}` }}
+				/>, heroes[player.hero_id].localized_name];
+}
+
+
+class JungleStory {
+  constructor(match) {
+    this.players = match.players.filter(player => (player.lane === 4 || player.lane === 5) && !player.is_roaming);
+    this.lane = 4;
+  }
+  static exists(match) {
+    return match.players.filter(player => (player.lane === 4 || player.lane === 5) && !player.is_roaming).length > 0;
+  }
+  format() {
+    return formatTemplate("{players} farmed the jungle", {
+      players: formatList(this.players.map(PlayerSpan)),
+    });
+  }
+}
+
+class RoamStory {
+  constructor(match) {
+    this.players = match.players.filter(player => player.is_roaming);
+    this.lane = 6;
+  }
+  static exists(match) {
+    return match.players.filter(player => player.is_roaming).length > 0;
+  }
+  format() {
+    return formatTemplate("{players} roamed", {
+      players: formatList(this.players.map(PlayerSpan)),
+    });
+  }
 }
 
 const localizedLane = {
-  1: "Bot",
-  2: "Mid",
-  3: "Top",
+	1: "Bot",
+	2: "Mid",
+	3: "Top",
 };
 
 const getLaneScore = players => (Math.max(...players.map(player => player.gold_t[10] || 0)) || 0);
 const laneScoreDraw = 500;
 
 class LaneStory {
-  constructor(match, lane) {
-    this.radiant_players = match.players.filter(player => player.lane == parseInt(lane, 10) && player.isRadiant && (!player.is_roaming));
-    this.dire_players = match.players.filter(player => player.lane == parseInt(lane, 10) && !player.isRadiant && (!player.is_roaming));
-    this.lane = lane;
-    this.winning_team = getLaneScore(this.radiant_players) > getLaneScore(this.dire_players);
-    this.is_draw = Math.abs(getLaneScore(this.radiant_players) - getLaneScore(this.dire_players)) <= laneScoreDraw;
-  }
-  format() {
-    // If there is nobody in this lane
-    if (this.radiant_players.length === 0 && this.dire_players.length === 0) {
-      return formatTemplate("there was nobody in {lane} lane", {
-        lane: localizedLane[this.lane],
-      });
-    }
-    // If only one team is in this lane
-    if (this.radiant_players.length === 0 || this.dire_players.length === 0) {
-      return formatTemplate("{players} had a free {lane} lane", {
-        players: formatList(this.radiant_players.concat(this.dire_players).map(PlayerSpan)),
-        lane: localizedLane[this.lane],
-      });
-    }
-    // If both teams are in this lane
+	constructor(match, lane) {
+		this.radiant_players = match.players.filter(player => player.lane == parseInt(lane, 10) && player.isRadiant && (!player.is_roaming));
+		this.dire_players = match.players.filter(player => player.lane == parseInt(lane, 10) && !player.isRadiant && (!player.is_roaming));
+		this.lane = lane;
+		this.winning_team = getLaneScore(this.radiant_players) > getLaneScore(this.dire_players);
+		this.is_draw = Math.abs(getLaneScore(this.radiant_players) - getLaneScore(this.dire_players)) <= laneScoreDraw;
+	}
+	format() {
+		// If there is nobody in this lane
+		if (this.radiant_players.length === 0 && this.dire_players.length === 0) {
+			return formatTemplate("there was nobody in {lane} lane", {
+				lane: localizedLane[this.lane],
+			});
+		}
+		// If only one team is in this lane
+		if (this.radiant_players.length === 0 || this.dire_players.length === 0) {
+			return formatTemplate("{players} had a free {lane} lane", {
+				players: formatList(this.radiant_players.concat(this.dire_players).map(PlayerSpan)),
+				lane: localizedLane[this.lane],
+			});
+		}
+		// If both teams are in this lane
 
-    // If it's close enough to be a draw
-    if (this.is_draw) {
-      return formatTemplate("{radiant_players} drew even in {lane} Lane with {dire_players}", {
-        radiant_players: formatList(this.radiant_players.map(PlayerSpan), "nobody"),
-        dire_players: formatList(this.dire_players.map(PlayerSpan), "nobody"),
-        lane: localizedLane[this.lane],
-      });
-    }
+		// If it's close enough to be a draw
+		if (this.is_draw) {
+			return formatTemplate("{radiant_players} drew even in {lane} Lane with {dire_players}", {
+				radiant_players: formatList(this.radiant_players.map(PlayerSpan), "nobody"),
+				dire_players: formatList(this.dire_players.map(PlayerSpan), "nobody"),
+				lane: localizedLane[this.lane],
+			});
+		}
 
-    // If one team won
-    return formatTemplate(this.winning_team ? "{radiant_players} won {lane} Lane against {dire_players}" : "{radiant_players} lost {lane} Lane to {dire_players}", {
-      radiant_players: formatList(this.radiant_players.map(PlayerSpan), "nobody"),
-      dire_players: formatList(this.dire_players.map(PlayerSpan), "nobody"),
-      lane: localizedLane[this.lane],
-    });
-  }
+		// If one team won
+		return formatTemplate(this.winning_team ? "{radiant_players} won {lane} Lane against {dire_players}" : "{radiant_players} lost {lane} Lane to {dire_players}", {
+			radiant_players: formatList(this.radiant_players.map(PlayerSpan), "nobody"),
+			dire_players: formatList(this.dire_players.map(PlayerSpan), "nobody"),
+			lane: localizedLane[this.lane],
+		});
+	}
 }
 
-class Lanes extends React.Component{
+class Lanes extends React.Component {
 	render() {
 		const {match} = this.props
-    const lanes = Object.keys(localizedLane).map(lane => new LaneStory(match, lane));
-    // if (JungleStory.exists(match)) {
-    //   lanes.push(new JungleStory(match));
-    // }
-    // if (RoamStory.exists(match)) {
-    //   lanes.push(new RoamStory(match));
-    // }
+		const lanes = Object.keys(localizedLane).map(lane => new LaneStory(match, lane));
+		if (JungleStory.exists(match)) {
+		  lanes.push(new JungleStory(match));
+		}
+		if (RoamStory.exists(match)) {
+		  lanes.push(new RoamStory(match));
+		}
 		return (
-			<View>
-				{lanes.map(lane => <View key={lane.lane}><Text>{lane.format()}</Text></View>)}
+			<View style={styles.highlight}>
+				<View>
+					<Text style={styles.header}>Lanes:</Text>
+				</View>
+				{lanes.map(lane => 
+					<View key={lane.lane}>
+						<Text>{lane.format()}</Text>
+					</View>)
+				}
 			</View>
 		);
 	}
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = state => ({})
 
-})
-
-const mapDispatchToProps = dispatch => ({
-})
+const mapDispatchToProps = dispatch => ({})
 
 export default connect(mapStateToProps,mapDispatchToProps)(Lanes);
